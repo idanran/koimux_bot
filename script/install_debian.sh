1a6c615e247b21c5986007a09c8fed0442d947e1
idanran <96647698+idanran@users.noreply.github.com>
2024-01-08 01:51:09 +0800

    Update install_debian.sh

--- script/install_debian.sh
+++ script/install_debian.sh
@@ -64,8 +64,7 @@ echo "$hostinfo" > $sys_name-$AH/etc/hostname
 echo "127.0.0.1 $hostinfo" > $sys_name-$AH/etc/hosts
 rm -rf $sys_name-$AH/etc/resolv.conf &&
 echo "nameserver 223.5.5.5
-nameserver 223.6.6.6
-nameserver 114.114.114.114" >$sys_name-$AH/etc/resolv.conf
+nameserver 223.6.6.6" >$sys_name-$AH/etc/resolv.conf
 echo "设置时区"
 sleep $SLEEP_TIME
 rm systeminfo.log
